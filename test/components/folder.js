2b66f77e7bd4b694d8bd95dbf2c63f08cf3f233b
StepnickaSantiago <stepnicka012@gmail.com>
2025-10-04 14:45:38 -0300

    Delete test/components directory

--- test/components/folder.js
+++
@@ -1,6 +0,0 @@
-const { MinecraftFolder } = require('../../dist/index');
-
-console.log("Default:", MinecraftFolder.default.getDefault());
-
-MinecraftFolder.default.setFolder("Steplauncher");
-console.log("Custom:", MinecraftFolder.default.getFolder());
